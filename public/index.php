053b3a315d67c659d36bfd2f31ab7d4a1583c45d
kruARaweewan <raweewan2010@gmail.com>
2014-05-11 15:34:09 +0700

    First Branch by Rawee

--- public/index.php
+++ public/index.php
@@ -1,10 +1,54 @@
 <?php
-echo "Hello index";
-echo "<br>";
-echo "Hello PHP";
-echo "<br>";
-echo "Hello PHP by YingRawee";
-echo "<br>";
-echo "Hello Team 2"; 
-echo "<hr>";
+
+try {
+
+    //Register an autoloader
+    $loader = new \Phalcon\Loader();
+    $loader->registerDirs(array(
+        '../app/controllers/',
+        '../app/models/'
+    ))->register();
+
+    //Create a DI
+    $di = new Phalcon\DI\FactoryDefault();
+    
+    //Setup the database service
+    $di->set('db', function(){
+    	return new \Phalcon\Db\Adapter\Pdo\Mysql(array(
+    			"host" => "localhost",
+    			"username" => "root",
+    			"password" => "",
+    			"dbname" => "estudent"
+    	));
+    });
+
+
+    	//Registering Volt as template engine
+    	$di->set('view', function() {
+    	
+    		$view = new \Phalcon\Mvc\View();
+    	
+    		$view->setViewsDir('../app/views/');
+    	
+    		$view->registerEngines(array(
+    				".volt" => 'Phalcon\Mvc\View\Engine\Volt'
+    		));
+    	
+    		return $view;
+    	});
+    //Setup a base URI so that all generated URIs include the "tutorial" folder
+    $di->set('url', function(){
+        $url = new \Phalcon\Mvc\Url();
+        $url->setBaseUri('/tutorial/');
+        return $url;
+    });
+
+    //Handle the request
+    $application = new \Phalcon\Mvc\Application($di);
+
+    echo $application->handle()->getContent();
+
+} 	catch (\Phalcon\Exception $e) {
+     echo "PhalconException: ", $e->getMessage();
+}
 ?>
